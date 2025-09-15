3fcddf945b113da4908f94b2083908ad56c55fbe
prateek kumar <chaturvediprateek474@gmail.com>
2025-09-15 21:56:04 +0530

    change in testimonial

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Hero from '../components/Hero'
 import FeaturedSection from '../components/FeaturedSection'
 import Banner from '../components/Banner'
-import Testimonials from '../components/testinomials'
+import Testimonials from '../components/Testinomials'
 import Newsletter from '../components/Newsletter'
 import Footer from '../components/Footer'
 
